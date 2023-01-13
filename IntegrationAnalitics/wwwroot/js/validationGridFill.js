29e2e3c58f0f1a9e1f4e1e056f266aa8ac9cb303
Alexandr <recovni96@gmail.com>
2023-01-13 16:54:49 +0500

    new branch

--- IntegrationAnalitics/wwwroot/js/validationGridFill.js
+++ IntegrationAnalitics/wwwroot/js/validationGridFill.js
@@ -2,11 +2,12 @@
     //console.log('<div class="template-areas-cell11"></div>');
     let div = document.createElement('div');
     div.className = "alert";
-    div.innerHTML = "<strong>Ы</strong>";
+    div.innerHTML = "<strong>1</strong>";
     await document.getElementById('validationGridFill').append(div);
     await console.log(document.getElementsByClassName('alert')[0])
-    await document.getElementsByClassName('alert')[0].insertAdjacentHTML('afterend', '<div class="bla">Пока</div>');
-    await document.getElementsByClassName('bla')[0].insertAdjacentHTML('afterend', '<div class="bla2">Привет</div>');
+    await document.getElementsByClassName('alert')[0].insertAdjacentHTML('afterend', '<div class="bla">2</div>');
+    await document.getElementsByClassName('bla')[0].insertAdjacentHTML('afterend', '<div class="bla2">3</div>');
+    await document.getElementsByClassName('bla2')[0].insertAdjacentHTML('afterend', '<div class="bla3">4</div>');
 
     //append(document.createElement('div'));
     //await .innerHTML = 'asd'
