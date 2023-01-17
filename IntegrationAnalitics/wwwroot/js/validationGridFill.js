5969b2577ba5ea0be55df1c987d7cf38a795ad25
Alexandr <recovni96@gmail.com>
2023-01-17 13:31:29 +0500

    update validation page

--- IntegrationAnalitics/wwwroot/js/validationGridFill.js
+++ IntegrationAnalitics/wwwroot/js/validationGridFill.js
@@ -1,14 +1,14 @@
 ﻿async function validationGridFill() {
-    //console.log('<div class="template-areas-cell11"></div>');
-    let div = document.createElement('div');
-    div.className = "alert";
-    div.innerHTML = "<strong>Ы</strong>";
-    await document.getElementById('validationGridFill').append(div);
-    await console.log(document.getElementsByClassName('alert')[0])
-    await document.getElementsByClassName('alert')[0].insertAdjacentHTML('afterend', '<div class="bla">Пока</div>');
-    await document.getElementsByClassName('bla')[0].insertAdjacentHTML('afterend', '<div class="bla2">Привет</div>');
+    let check2_circle = "<svg xmlns=\"http://www.w3.org/2000/svg\" color=\"green\" width=\"24\" height=\"24\" fill=\"currentColor\" class=\"bi bi-check2-circle\" viewBox=\"0 0 16 16\">+\n" +
+        "<path d=\"M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z\"/>+\n" +
+        "<path d=\"M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z\"/>+\n" +
+        "</svg>";
 
-    //append(document.createElement('div'));
-    //await .innerHTML = 'asd'
-    await document.getElementById('validationGridFill').append(div);
+    let row = ["1. agreement.xsd - agreement.xml","15:06 21.12.2022","15:06 21.12.2022",check2_circle,check2_circle];
+
+    for (let i = 0; i <= 49; i++) {
+        for(let n = 0; n <= 4; n++){
+            await document.getElementsByClassName('panel-scroll-grid')[0].insertAdjacentHTML('beforeend', '<div class="cell">'+row[n]+'</div>');
+        }
+    }
 }
