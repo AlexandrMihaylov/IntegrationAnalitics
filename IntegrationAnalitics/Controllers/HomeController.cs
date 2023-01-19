7fdad6c3f9c3bdb11e0c0802a52e6fd2a637b9fc
Alexandr <recovni96@gmail.com>
2023-01-19 14:20:39 +0500

    1. uploading add single-switch api/smev 2. uploading api+smev left block height from window

--- IntegrationAnalitics/Controllers/HomeController.cs
+++ IntegrationAnalitics/Controllers/HomeController.cs
@@ -23,8 +23,22 @@ public class HomeController : Controller
         return View("FaqSearch");
     }
 
+    //<?xml version="1.0" encoding="UTF-8"?>
+    // <UNPDocument
+    //     xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    // xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    // xmlns="urn://x-artefacts-minfin-unp/1.13.0">
+    // <DocumentNumber>148</DocumentNumber>
+    // <DocumentType>9</DocumentType>
+    // <CreateNew>true</CreateNew>
+    // </UNPDocument>
+    //https://unp.bars.group/release/api/smev
     public IActionResult Uploading()
     {
+        //TODO : Алгоритм выгрузки по смеву
+        //TODO : Задать начальные перменные для старта выгрузки после прописать алгоритм запросов выгрузки
+        //TODO : Получить текст документа, отправить его на фронт, + сделать возможность скачивания
+        
         return View("Uploading");
     }
     
