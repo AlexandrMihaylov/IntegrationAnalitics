d6897f2d2ed830bfcc266c343fa7c2739ce8b072
Alexandr <recovni96@gmail.com>
2023-01-20 11:14:12 +0500

    add func GetXmlHandler

--- IntegrationAnalitics/Controllers/HomeController.cs
+++ IntegrationAnalitics/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using IntegrationAnalitics.Models;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace IntegrationAnalitics.Controllers;
 
@@ -35,13 +39,27 @@ public class HomeController : Controller
     //https://unp.bars.group/release/api/smev
     public IActionResult Uploading()
     {
+        async void GetXmlHandler()
+        {
+            using var client = new HttpClient();
+            string request = JsonConvert.SerializeObject(new Dictionary<string, object> { {"param", "<?xml version='1.0' encoding='UTF-8'?><UNPDocument xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='urn://x-artefacts-minfin-unp/1.13.0'><DocumentNumber>148</DocumentNumber><DocumentType>9</DocumentType><CreateNew>true</CreateNew></UNPDocument>"} });
+            var stringContent = new StringContent(request);
+            var result = await client.PostAsync("https://unp.bars.group/release/api/smev", stringContent);
+            using (var sr = new StreamReader(await result.Content.ReadAsStreamAsync(), Encoding.GetEncoding("iso-8859-1")))
+            {
+                string s = sr.ReadToEnd();
+                Console.WriteLine(s.Substring(0, 1000));
+            }
+        }
+        GetXmlHandler();
+
         //TODO : Алгоритм выгрузки по смеву
         //TODO : Задать начальные перменные для старта выгрузки после прописать алгоритм запросов выгрузки
         //TODO : Получить текст документа, отправить его на фронт, + сделать возможность скачивания
-        
+
         return View("Uploading");
     }
-    
+
     public IActionResult Validation()
     {
         return View("Validation");
